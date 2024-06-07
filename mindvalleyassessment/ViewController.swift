4f3acb23d9cfe440e6587893136bca0edef7a898
Alwyn Yeo <alwynyeo98@gmail.com>
2024-06-07 22:33:09 +0800

    Initial Commit

---
+++ mindvalleyassessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mindvalleyassessment
+//
+//  Created by Alwyn Yeo on 6/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
