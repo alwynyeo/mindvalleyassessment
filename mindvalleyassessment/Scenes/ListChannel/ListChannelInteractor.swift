dda70633f6c706be79045ed1b9dbe599c502f595
Alwyn Yeo <alwynyeo98@gmail.com>
2024-06-12 17:16:05 +0800

    feat(interactor): request data from worker
    - communicate with worker to fetch data

--- mindvalleyassessment/Scenes/ListChannel/ListChannelInteractor.swift
+++ mindvalleyassessment/Scenes/ListChannel/ListChannelInteractor.swift
@@ -6,9 +6,11 @@
 //  Copyright (c) 2024 ___ORGANIZATIONNAME___. All rights reserved.
 //
 
+import Foundation
+
 // MARK: - ListChannelBusinessLogic Protocol
 protocol ListChannelBusinessLogic {
-    func doSomething(request: ListChannel.Something.Request)
+    func loadData(request: ListChannel.LoadData.Request)
 }
 
 // MARK: - ListChannelDataStore Protocol
@@ -23,6 +25,13 @@ final class ListChannelInteractor {
     var presenter: ListChannelPresentationLogic?
     
     var worker: ListChannelWorker?
+
+    private var newEpisodeData: NewEpisode?
+
+    private var channelData: Channel?
+
+    private var categoryData: Category?
+
     //  var name: String = ""
 
     // MARK: - Object Lifecycle
@@ -31,22 +40,71 @@ final class ListChannelInteractor {
         let service = NetworkService.shared
         worker = ListChannelWorker(service: service)
     }
+
+    // MARK: - Helpers
 }
 
 // MARK: - ListChannelBusinessLogic Extension
 extension ListChannelInteractor: ListChannelBusinessLogic {
-    func doSomething(request: ListChannel.Something.Request) {
-        worker?.getEpisodes { result in
+    func loadData(request: ListChannel.LoadData.Request) {
+        let dispatchGroup = DispatchGroup()
+
+        dispatchGroup.enter()
+
+        worker?.getEpisodes { [weak self] result in
+            guard let self else { return }
+
             switch result {
                 case .success(let data):
-                    let newEpisodes = data.data.media
-                    print("New Episodes", newEpisodes.count)
+                    newEpisodeData = data
                 case .failure(let error):
-                    print("Error::", error.localizedDescription)
+                    print("Worker GetEpisodes Error::", error.localizedDescription)
             }
+
+            dispatchGroup.leave()
         }
-//        let response = ListChannel.Something.Response()
-//        presenter?.presentSomething(response: response)
+
+        dispatchGroup.enter()
+
+        worker?.getChannels { [weak self] result in
+            guard let self else { return }
+
+            switch result {
+                case .success(let data):
+                    channelData = data
+                case .failure(let error):
+                    print("Worker GetChannels Error::", error.localizedDescription)
+            }
+
+            dispatchGroup.leave()
+        }
+
+        dispatchGroup.enter()
+
+        worker?.getCategories { [weak self] result in
+            guard let self else { return }
+
+            switch result {
+                case .success(let data):
+                    categoryData = data
+                case .failure(let error):
+                    print("Worker GetCategories Error::", error.localizedDescription)
+            }
+
+            dispatchGroup.leave()
+        }
+
+        dispatchGroup.notify(queue: DispatchQueue.main) { [weak self] in
+            guard let self else { return }
+
+            let response = ListChannel.LoadData.Response(
+                newEpisodeData: newEpisodeData,
+                channelData: channelData,
+                categoryData: categoryData
+            )
+
+            presenter?.presentLoadedData(response: response)
+        }
     }
 }
 
