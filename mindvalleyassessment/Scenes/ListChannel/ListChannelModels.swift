6a14028b6b85a1f4b83744c1481046ba9be89ea7
Alwyn Yeo <alwynyeo98@gmail.com>
2024-06-12 17:17:55 +0800

    feat(scene model): update models

--- mindvalleyassessment/Scenes/ListChannel/ListChannelModels.swift
+++ mindvalleyassessment/Scenes/ListChannel/ListChannelModels.swift
@@ -6,13 +6,53 @@
 //  Copyright (c) 2024 ___ORGANIZATIONNAME___. All rights reserved.
 //
 
+import Foundation
+
 // MARK: - ListChannel Enum
 enum ListChannel {
-    enum Something {
+    enum LoadData {
         struct Request {}
-        
-        struct Response {}
-        
-        struct ViewModel {}
+
+        struct Response {
+            let newEpisodeData: NewEpisode?
+            let channelData: Channel?
+            let categoryData: Category?
+        }
+
+        struct ViewModel {
+            let sections: [Section]
+        }
+    }
+
+    struct Section: Hashable {
+        let id: String
+        var imageUrl: URL? = nil
+        let title: String
+        var subTitle: String? = nil
+        let items: [Item]
+        var isSeriesType: Bool = false
+
+        init(imageUrl: URL? = nil, title: String, subTitle: String? = nil, items: [Item], isSeriesType: Bool = false) {
+            self.id = UUID().uuidString
+            self.imageUrl = imageUrl
+            self.title = title
+            self.subTitle = subTitle
+            self.items = items
+            self.isSeriesType = isSeriesType
+        }
+
+        struct Item: Hashable {
+            let id: String
+            var imageUrl: URL? = nil
+            let title: String
+            var subTitle: String? = nil
+
+            init(imageUrl: URL? = nil, title: String, subTitle: String? = nil) {
+                self.id = UUID().uuidString
+                self.imageUrl = imageUrl
+                self.title = title
+                self.subTitle = subTitle
+            }
+        }
     }
 }
