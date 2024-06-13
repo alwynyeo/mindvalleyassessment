0d11e44623a60e1a541046520c8ea23fece602c7
Alwyn Yeo <alwynyeo98@gmail.com>
2024-06-14 00:09:55 +0800

    feat(worker): introduce persistence service

--- mindvalleyassessment/Scenes/ListChannel/ListChannelWorker.swift
+++ mindvalleyassessment/Scenes/ListChannel/ListChannelWorker.swift
@@ -10,41 +10,54 @@ import Foundation
 
 // MARK: - ListChannelServiceProtocol Protocol
 protocol ListChannelServiceProtocol {
-    func getNewEpisodes(from url: URL, completion: @escaping (NewEpisodeResultType) -> Void)
-    func getChannels(from url: URL, completion: @escaping (ChannelResultType) -> Void)
-    func getCategories(from url: URL, completion: @escaping (CategoryResultType) -> Void)
+    func getNewEpisodes(completion: @escaping (NewEpisodeResultType) -> Void)
+    func getChannels(completion: @escaping (ChannelResultType) -> Void)
+    func getCategories(completion: @escaping (CategoryResultType) -> Void)
 }
 
 // MARK: - ListChannelWorker Class
 final class ListChannelWorker {
     // MARK: - Declarations
 
-    private let service: ListChannelServiceProtocol
+    private let networkService: ListChannelServiceProtocol
 
-    private var urlComponents: URLComponents = {
-        var components = URLComponents()
-        components.scheme = "https"
-        components.host = "pastebin.com"
-        return components
-    }()
+    private let persistenceService: ListChannelServiceProtocol
 
     // MARK: - Object Lifecycle
 
-    init(service: ListChannelServiceProtocol) {
-        self.service = service
+    init(
+        networkService: ListChannelServiceProtocol,
+        persistenceService: ListChannelServiceProtocol
+    ) {
+        self.networkService = networkService
+        self.persistenceService = persistenceService
     }
 
     // MARK: - Helpers
 
-    func getEpisodes(completion: @escaping (NewEpisodeResultType) -> Void) {
-        urlComponents.path = "/raw/z5AExTtw"
-
-        guard let url = urlComponents.url else {
-            completion(NewEpisodeResultType.failure(NetworkError.invalidUrl))
-            return
+    private func persistNewEpisode(data: NewEpisode) {
+        DispatchQueue.main.async {
+            PersistenceService.shared.persist(newEpisode: data)
         }
+    }
 
-        service.getNewEpisodes(from: url) { result in
+    private func persistChannel(data: Channel) {
+        DispatchQueue.main.async {
+            PersistenceService.shared.persist(channel: data)
+        }
+    }
+
+    private func persistCategory(data: Category) {
+        DispatchQueue.main.async {
+            PersistenceService.shared.persist(category: data)
+        }
+    }
+}
+
+// MARK: - Local Data Related Helpers
+extension ListChannelWorker {
+    func getLocalNewEpisodes(completion: @escaping (NewEpisodeResultType) -> Void) {
+        persistenceService.getNewEpisodes { result in
             switch result {
                 case .success(let data):
                     let result = NewEpisodeResultType.success(data)
@@ -56,15 +69,8 @@ final class ListChannelWorker {
         }
     }
 
-    func getChannels(completion: @escaping (ChannelResultType) -> Void) {
-        urlComponents.path = "/raw/Xt12uVhM"
-
-        guard let url = urlComponents.url else {
-            completion(ChannelResultType.failure(NetworkError.invalidUrl))
-            return
-        }
-
-        service.getChannels(from: url) { result in
+    func getLocalChannels(completion: @escaping (ChannelResultType) -> Void) {
+        persistenceService.getChannels { result in
             switch result {
                 case .success(let data):
                     let result = ChannelResultType.success(data)
@@ -76,17 +82,68 @@ final class ListChannelWorker {
         }
     }
 
-    func getCategories(completion: @escaping (CategoryResultType) -> Void) {
-        urlComponents.path = "/raw/A0CgArX3"
-
-        guard let url = urlComponents.url else {
-            completion(CategoryResultType.failure(NetworkError.invalidUrl))
-            return
-        }
-
-        service.getCategories(from: url) { result in
+    func getLocalCategories(completion: @escaping (CategoryResultType) -> Void) {
+        persistenceService.getCategories { result in
             switch result {
                 case .success(let data):
+                    let result = CategoryResultType.success(data)
+                    completion(result)
+                case .failure(let error):
+                    let result = CategoryResultType.failure(error)
+                    completion(result)
+            }
+        }
+    }
+}
+
+// MARK: - Cloud Data Related Helpers
+extension ListChannelWorker {
+    func getCloudNewEpisodes(completion: @escaping (NewEpisodeResultType) -> Void) {
+        networkService.getNewEpisodes { [weak self] result in
+            guard let self else {
+                print("self is nil under \(#function) at line \(#line) in \(#fileID) file.")
+                return
+            }
+            switch result {
+                case .success(let data):
+                    persistNewEpisode(data: data)
+                    let result = NewEpisodeResultType.success(data)
+                    completion(result)
+                case .failure(let error):
+                    let result = NewEpisodeResultType.failure(error)
+                    completion(result)
+            }
+        }
+    }
+
+    func getCloudChannels(completion: @escaping (ChannelResultType) -> Void) {
+        networkService.getChannels { [weak self] result in
+            guard let self else {
+                print("self is nil under \(#function) at line \(#line) in \(#fileID) file.")
+                return
+            }
+            switch result {
+                case .success(let data):
+                    persistChannel(data: data)
+                    let result = ChannelResultType.success(data)
+                    completion(result)
+                case .failure(let error):
+                    let result = ChannelResultType.failure(error)
+                    completion(result)
+            }
+        }
+    }
+
+    func getCloudCategories(completion: @escaping (CategoryResultType) -> Void) {
+        networkService.getCategories { [weak self] result in
+            guard let self else {
+                print("self is nil under \(#function) at line \(#line) in \(#fileID) file.")
+                return
+            }
+
+            switch result {
+                case .success(let data):
+                    persistCategory(data: data)
                     let result = CategoryResultType.success(data)
                     completion(result)
                 case .failure(let error):
