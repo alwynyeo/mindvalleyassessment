ac2400b586333e1c8add51f43ac61732d4179d04
Alwyn Yeo <alwynyeo98@gmail.com>
2024-06-14 00:12:24 +0800

    feat(interactor): introduce persistence service

--- mindvalleyassessment/Scenes/ListChannel/ListChannelInteractor.swift
+++ mindvalleyassessment/Scenes/ListChannel/ListChannelInteractor.swift
@@ -14,16 +14,14 @@ protocol ListChannelBusinessLogic {
 }
 
 // MARK: - ListChannelDataStore Protocol
-protocol ListChannelDataStore {
-    //  var name: String { get set }
-}
+protocol ListChannelDataStore {}
 
 // MARK: - ListChannelInteractor Class
 final class ListChannelInteractor {
     // MARK: - Declarations
 
     var presenter: ListChannelPresentationLogic?
-    
+
     var worker: ListChannelWorker?
 
     private var newEpisodeData: NewEpisode?
@@ -32,57 +30,201 @@ final class ListChannelInteractor {
 
     private var categoryData: Category?
 
-    //  var name: String = ""
+    private let localDataDispatchGroup: DispatchGroup
+
+    private let cloudDataDispatchGroup: DispatchGroup
 
     // MARK: - Object Lifecycle
 
     init() {
-        let service = NetworkService.shared
-        worker = ListChannelWorker(service: service)
+        let networkService = NetworkService.shared
+        let persistenceService = PersistenceService.shared
+        let worker = ListChannelWorker(
+            networkService: networkService,
+            persistenceService: persistenceService
+        )
+        let localDataDispatchGroup = DispatchGroup()
+        let cloudDataDispatchGroup = DispatchGroup()
+        self.worker = worker
+        self.localDataDispatchGroup = localDataDispatchGroup
+        self.cloudDataDispatchGroup = cloudDataDispatchGroup
     }
 
     // MARK: - Helpers
+
+    private func loadLocalData() {
+        print("run data from core data")
+        loadLocalNewEpisodes()
+        loadLocalChannels()
+        loadLocalCategories()
+
+        localDataDispatchGroup.notify(queue: DispatchQueue.main) { [weak self] in
+            guard let self else {
+                print("self is nil under \(#function) at line \(#line) in \(#fileID) file.")
+                return
+            }
+
+            let response = ListChannel.LoadData.Response(
+                newEpisodeData: newEpisodeData,
+                channelData: channelData,
+                categoryData: categoryData
+            )
+
+            print("format local data")
+            presenter?.presentLoadedData(response: response)
+
+            // MARK: Load the latest data from the cloud
+            loadCloudData()
+        }
+    }
+
+    private func loadCloudData() {
+        print("run data from network")
+        loadCloudNewEpisodes()
+        loadCloudChannels()
+        loadCloudCategories()
+
+        cloudDataDispatchGroup.notify(queue: DispatchQueue.main) { [weak self] in
+            guard let self else {
+                print("self is nil under \(#function) at line \(#line) in \(#fileID) file.")
+                return
+            }
+
+            let response = ListChannel.LoadData.Response(
+                newEpisodeData: newEpisodeData,
+                channelData: channelData,
+                categoryData: categoryData
+            )
+
+            print("format cloud data")
+            presenter?.presentLoadedData(response: response)
+        }
+    }
 }
 
 // MARK: - ListChannelBusinessLogic Extension
 extension ListChannelInteractor: ListChannelBusinessLogic {
     func loadData(request: ListChannel.LoadData.Request) {
-        let dispatchGroup = DispatchGroup()
+        loadLocalData()
+    }
+}
 
-        dispatchGroup.enter()
+// MARK: - ListChannelDataStore Extension
+extension ListChannelInteractor: ListChannelDataStore {}
 
-        worker?.getEpisodes { [weak self] result in
-            guard let self else { return }
+// MARK: - Load Local Respective Data Helpers
+private extension ListChannelInteractor {
+    func loadLocalNewEpisodes() {
+        localDataDispatchGroup.enter()
+
+        worker?.getLocalNewEpisodes { [weak self] result in
+            guard let self else {
+                print("self is nil under \(#function) at line \(#line) in \(#fileID) file.")
+                return
+            }
 
             switch result {
                 case .success(let data):
                     newEpisodeData = data
                 case .failure(let error):
-                    print("Worker GetEpisodes Error::", error.localizedDescription)
+                    print("Worker LocalGetNewEpisodes Error::", error.localizedDescription)
             }
 
-            dispatchGroup.leave()
+            localDataDispatchGroup.leave()
         }
+    }
 
-        dispatchGroup.enter()
+    func loadLocalChannels() {
+        localDataDispatchGroup.enter()
 
-        worker?.getChannels { [weak self] result in
-            guard let self else { return }
+        worker?.getLocalChannels { [weak self] result in
+            guard let self else {
+                print("self is nil under \(#function) at line \(#line) in \(#fileID) file.")
+                return
+            }
 
             switch result {
                 case .success(let data):
                     channelData = data
                 case .failure(let error):
-                    print("Worker GetChannels Error::", error.localizedDescription)
+                    print("Worker LocalGetChannels Error::", error.localizedDescription)
             }
 
-            dispatchGroup.leave()
+            localDataDispatchGroup.leave()
         }
+    }
 
-        dispatchGroup.enter()
+    func loadLocalCategories() {
+        localDataDispatchGroup.enter()
 
-        worker?.getCategories { [weak self] result in
-            guard let self else { return }
+        worker?.getLocalCategories { [weak self] result in
+            guard let self else {
+                print("self is nil under \(#function) at line \(#line) in \(#fileID) file.")
+                return
+            }
+
+            switch result {
+                case .success(let data):
+                    categoryData = data
+                case .failure(let error):
+                    print("Worker LocalGetCategories Error::", error.localizedDescription)
+            }
+
+            localDataDispatchGroup.leave()
+        }
+    }
+}
+
+// MARK: - Load Cloud Data Related Helpers
+private extension ListChannelInteractor {
+    func loadCloudNewEpisodes() {
+        cloudDataDispatchGroup.enter()
+
+        worker?.getCloudNewEpisodes { [weak self] result in
+            guard let self else {
+                print("self is nil under \(#function) at line \(#line) in \(#fileID) file.")
+                return
+            }
+
+            switch result {
+                case .success(let data):
+                    newEpisodeData = data
+                case .failure(let error):
+                    print("Worker CloudGetNewEpisodes Error::", error.localizedDescription)
+            }
+
+            cloudDataDispatchGroup.leave()
+        }
+    }
+
+    func loadCloudChannels() {
+        cloudDataDispatchGroup.enter()
+
+        worker?.getCloudChannels { [weak self] result in
+            guard let self else {
+                print("self is nil under \(#function) at line \(#line) in \(#fileID) file.")
+                return
+            }
+
+            switch result {
+                case .success(let data):
+                    channelData = data
+                case .failure(let error):
+                    print("Worker CloudGetChannels Error::", error.localizedDescription)
+            }
+
+            cloudDataDispatchGroup.leave()
+        }
+    }
+
+    func loadCloudCategories() {
+        cloudDataDispatchGroup.enter()
+
+        worker?.getCloudCategories { [weak self] result in
+            guard let self else {
+                print("self is nil under \(#function) at line \(#line) in \(#fileID) file.")
+                return
+            }
 
             switch result {
                 case .success(let data):
@@ -91,22 +233,7 @@ extension ListChannelInteractor: ListChannelBusinessLogic {
                     print("Worker GetCategories Error::", error.localizedDescription)
             }
 
-            dispatchGroup.leave()
-        }
-
-        dispatchGroup.notify(queue: DispatchQueue.main) { [weak self] in
-            guard let self else { return }
-
-            let response = ListChannel.LoadData.Response(
-                newEpisodeData: newEpisodeData,
-                channelData: channelData,
-                categoryData: categoryData
-            )
-
-            presenter?.presentLoadedData(response: response)
+            cloudDataDispatchGroup.leave()
         }
     }
 }
-
-// MARK: - ListChannelDataStore Extension
-extension ListChannelInteractor: ListChannelDataStore {}
